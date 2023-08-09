03215bc3f659adbecb0259a9a04ef4709b1a12dc
aemorozov <admin@aemorozov.com>
2023-08-09 15:29:39 +0400

    fix address

--- src/pages/cotact/index.jsx
+++ src/pages/cotact/index.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
               <div className="contact_list_item_email_location">
                 <div className="contact_list_item_location">
                   <p>Office Locations:</p>
-                  <span>USA: 550 West B Street, 4th Floor, San Diego, CA 92101</span>
+                  <span>Canada: 100 King St West, Suite 5700, Toronto, Ontario M5X 1C7 </span>
                   <div className="contact_list_item_location_icon">
                     <span className="icon-mi_location"></span>
                   </div>
