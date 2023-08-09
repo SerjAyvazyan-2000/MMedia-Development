ef862e9502629bc5dc8aedf786fb70de6d445df4
aemorozov <admin@aemorozov.com>
2023-08-09 15:13:28 +0400

    delete underline

--- src/components/servicesItem/index.jsx
+++ src/components/servicesItem/index.jsx
@@ -1,38 +1,33 @@
-import "./style.scss"
-import React, {useState} from 'react';
+import "./style.scss";
+import React, { useState } from "react";
 
-
-const ServicesItem =   ({item,index,handleClick}) => {
-    return <div className="services_item_content">
-        {item.reverse ? <>
-                <div  onClick={()=>handleClick(index,item)} className={ "services_item"}>
-                    <div className="services_item_text">
-                        <p>  {item.title}</p>
-                    </div>
-                    <div className="services_item_img">
-                        <img src={item.img} alt=""/>
-                    </div>
-                </div>
-
-
-
-            </> :
-            <>
-                <div   onClick={()=>handleClick(index,item)} className={"services_item "}>
-                    <div className="services_item_img">
-                        <img src={item.img} alt=""/>
-                    </div>
-                    <div className="services_item_text">
-                        <p>  {item.title}</p>
-                    </div>
-                </div>
-
-            </>
-
-
-        }
-
-
+const ServicesItem = ({ item, index, handleClick }) => {
+  return (
+    <div className="services_item_content">
+      {item.reverse ? (
+        <>
+          <div onClick={() => handleClick(index, item)} className={"services_item"}>
+            <div className="services_item_text">
+              <p> {item.title}</p>
+            </div>
+            <div className="services_item_img">
+              <img src={item.img} alt="" />
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div onClick={() => handleClick(index, item)} className={"services_item "}>
+            <div className="services_item_img">
+              <img src={item.img} alt="" />
+            </div>
+            <div className="services_item_text">
+              <p> {item.title}</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-}
-export default ServicesItem
+  );
+};
+export default ServicesItem;
